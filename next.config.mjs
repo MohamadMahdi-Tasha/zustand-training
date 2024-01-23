074954932b70572648ea5d6fd38c4f971b962f69
Mahdi Tasha <imwhdiiii@gmail.com>
2024-01-23 16:05:09 +0330

    removed files and added some comments in some files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// Codes by mahdi tasha
+// Defining type of nextJS configss
 /** @type {import('next').NextConfig} */
+
+// Defining configs of nextJS
 const nextConfig = {};
 
+// Exporting configs of nextJS
 export default nextConfig;
